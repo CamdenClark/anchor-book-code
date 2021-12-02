48930c1c685439e7f72c3f2d6ea6962699c59bcd
Camden <camdenaws@gmail.com>
2021-12-01 23:56:36 -0800

    Simple counter code for access control

--- simple-counter-4/tests/simple-counter.ts
+++ simple-counter-4/tests/simple-counter.ts
@@ -32,10 +32,33 @@ describe("simple-counter", () => {
   it("Increments the counter", async () => {
     const counter = await initializeCounter(2);
 
-    await program.rpc.increment({ accounts: { counter } });
+    await program.rpc.increment({
+      accounts: { counter, authority: provider.wallet.publicKey },
+    });
 
     const counterData = await program.account.counter.fetch(counter);
 
     assert.ok(counterData.count.eq(new anchor.BN(3)));
+  });
+  it("Another user trying to increment throws an error", async () => {
+    const counter = await initializeCounter(2);
+
+    let transactionFailed = false;
+
+    const anotherUser = anchor.web3.Keypair.generate();
+
+    try {
+      await program.rpc.increment({
+        accounts: {
+          counter,
+          authority: anotherUser.publicKey,
+        },
+        signers: [anotherUser],
+      });
+    } catch {
+      transactionFailed = true;
+    }
+
+    assert.ok(transactionFailed);
   });
 });
